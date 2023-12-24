bc17aa5dd93a9bdc385cb129c5e8d0498913dd73
Alessandro Legnani <alegnani@ethz.ch>
2023-12-24 12:36:55 +0100

    documented utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+//! Various generic utilities
+
 use std::ops::{Add, Neg, Sub};
 
 use serde::Deserialize;
@@ -5,6 +7,7 @@ use serde_repr::Deserialize_repr;
 
 use crate::entities::Priority;
 
+/// Position of an entity
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Deserialize)]
 pub struct Position<T> {
     pub x: T,
@@ -15,6 +18,7 @@ impl<T> Position<T>
 where
     T: Add<Output = T> + Sub<Output = T> + Copy,
 {
+    /// Create new `Position` shifted in a given direction
     pub fn shift(&self, direction: Direction, distance: T) -> Self {
         let x = self.x;
         let y = self.y;
@@ -42,6 +46,10 @@ where
     }
 }
 
+/// Direction of an entity
+///
+/// Represented as a C-like enum as used in the Factorio blueprint JSON.
+/// The odd numbers are used for directions that go diagonally.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize_repr)]
 #[repr(u8)]
 pub enum Direction {
@@ -52,6 +60,7 @@ pub enum Direction {
 }
 
 impl Direction {
+    /// Returns a new `Direction` rotated in the given direction
     pub fn rotate(&self, direction: Rotation, amount: u8) -> Self {
         let incr = match direction {
             Rotation::Clockwise => 2,
@@ -61,6 +70,7 @@ impl Direction {
         new_u8.into()
     }
 
+    /// Returns a new `Direction` rotate to the given side
     pub fn rotate_side(&self, side: Priority) -> Self {
         match side {
             Priority::None => *self,
@@ -69,6 +79,7 @@ impl Direction {
         }
     }
 
+    /// Returns a new `Direction` pointing in the opposite direction
     pub fn flip(&self) -> Self {
         self.rotate(Rotation::Clockwise, 2)
     }
@@ -86,12 +97,15 @@ impl From<u8> for Direction {
     }
 }
 
+/// Rotation direction enum
 pub enum Rotation {
     Clockwise,
     Anticlockwise,
 }
 
-#[repr(u8)]
+/// Generic enum indicating the side
+///
+/// Used in IR edges and IR splitters/mergers to indicate the priority of a given edge.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Side {
     Left,
@@ -100,6 +114,7 @@ pub enum Side {
 }
 
 impl Side {
+    /// Returns `true` if the side is a `None` value.
     pub fn is_none(&self) -> bool {
         *self == Self::None
     }
